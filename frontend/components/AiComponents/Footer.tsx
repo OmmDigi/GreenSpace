52697165b901eb20ece4934d670068caf1580c0f
ashiana-omm <ashianainteriordesignkolkata@gmail.com>
2025-07-11 16:09:00 +0530

    add social media icons

--- frontend/components/AiComponents/Footer.tsx
+++ frontend/components/AiComponents/Footer.tsx
@@ -2,8 +2,12 @@ import { cn } from "@/utils/cn";
 import Link from "next/link";
 import OpenGetQuoteDialog from "../Utils/OpenGetQuoteDialog";
 import { SERVICES } from "@/constant";
+import { Facebook, Youtube, Instagram } from "lucide-react";
 
-const FOOTER_LINKS = [
+const FOOTER_LINKS: {
+  heading: string;
+  links: { text: string; slug: string; icon?: React.ReactNode }[];
+}[] = [
   {
     heading: "Services",
     links: SERVICES.map((item) => ({
@@ -83,14 +87,17 @@ const FOOTER_LINKS = [
       {
         text: "Facebook",
         slug: "https://www.facebook.com/greenspaceinterior2023/",
+        icon: <Facebook size={18} />,
       },
       {
         text: "Instagram",
         slug: "https://www.instagram.com/greenspaceinterior23",
+        icon: <Instagram size={18} />,
       },
       {
         text: "YouTube",
         slug: "#",
+        icon: <Youtube size={18} />,
       },
     ],
   },
@@ -113,12 +120,29 @@ export default function Footer() {
 
         <div className="grid md:grid-cols-4 gap-8 text-sm max-sm:grid-cols-2">
           {FOOTER_LINKS.map((links, index) => (
-            <div key={index} className={index === 0 ? "max-sm:order-3" : index === 1 ? "max-sm:order-4" : index === 2 ? "max-sm:order-1" : "max-sm:order-2"}>
+            <div
+              key={index}
+              className={
+                index === 0
+                  ? "max-sm:order-3"
+                  : index === 1
+                  ? "max-sm:order-4"
+                  : index === 2
+                  ? "max-sm:order-1"
+                  : "max-sm:order-2"
+              }
+            >
               <h3 className={cn("font-semibold mb-4")}>{links.heading}</h3>
               <ul className={cn("space-y-2 text-teal-100")}>
                 {links.links.map((item, index) => (
                   <li key={index}>
                     <Link href={item.slug} className={cn("hover:text-white")}>
+                      {item.icon ? (
+                        <span className="float-left mt-[1px] mr-1.5">
+                          {item.icon}
+                        </span>
+                      ) : null}
+
                       {item.text}
                     </Link>
                   </li>
